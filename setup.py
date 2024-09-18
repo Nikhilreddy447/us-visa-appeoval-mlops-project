f32cfec26b94ceb33c4ce5e28c7b70ef12dfd8e5
nikhilreddy447 <aletinikhilreddy759@gmail.com>
2024-09-18 12:16:25 +0530

    implemented setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= "us-visa",
+    version= "0.0.0",
+    author="nikhil",
+    author_email="nikhilreddyaleti2002@gmail.com",
+    packages=find_packages()
+)
